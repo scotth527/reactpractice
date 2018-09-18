9efb58a04fafa8117789b8d60913d28d014f5a93
Scott Huang <scottyh527@gmail.com>
2018-09-18 00:17:20 +0000

    added nav shell

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,36 @@ import 'bootstrap';
 import '../styles/index.scss';
 
 //import your own components
-import {Home} from './component/home.jsx';
+function NavBar (props) {
+    return (<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+    </nav>
+        );
+}
+
+
 
 //render your react application
 ReactDOM.render(
-    <Home />,
+    <NavBar />,
     document.querySelector('#app')
 );
