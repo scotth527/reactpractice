a88aa6b107053ad6d523fc9a4735267a962c89fc
Scott Huang <scottyh527@gmail.com>
2018-09-18 01:56:44 +0000

    fixed nav bar

--- src/js/index.js
+++ src/js/index.js
@@ -9,28 +9,32 @@ import 'bootstrap';
 import '../styles/index.scss';
 
 //import your own components
-function NavBar (props) {
-    return (<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
-  </div>
+function NavBar () {
+    return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Start Bootstrap</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <span className="navbar-text ml-auto">
+            <div className="collapse navbar-collapse " id="navbarText">
+                <ul className="navbar-nav ">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>    
+            </div>
+        </span>
+        
     </nav>
         );
 }
